50759a882f7dd0c322b9fff9c7fbf56a2cb6f15b
Pizexel <110903734+Pizexel@users.noreply.github.com>
2024-01-06 11:17:09 +0300

    project1.py

--- project1.py
+++ project1.py
@@ -1,1 +1,30 @@
+from transformers import pipeline
+import wikipedia
 
+def processor(question, text):
+    # Create a question answering pipeline
+    qa = pipeline("question-answering")
+
+    # Get the answer from the pipeline
+    answer = qa(question=question, context=text)
+
+    # Print the answer
+    print(answer)
+
+wikipedia.set_lang("en")
+
+print("Welcome! Ask me anything.")
+
+# get user input
+user_input = "what causes precipitation to fall"
+
+# search for query on Wikipedia
+try:
+    page = wikipedia.page(user_input)
+    text = page.summary
+    question = user_input
+    processor(question, text)
+except wikipedia.exceptions.DisambiguationError as e:
+    print("Can you please be more specific?")
+except wikipedia.exceptions.PageError as e:
+    print("Sorry, I could not find any information on that topic.")
